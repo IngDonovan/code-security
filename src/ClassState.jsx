f297d01d7ac50cd693b20c8aee2225dffbb43e0d
Donovan Rojas <rojasdrm03@gmail.com>
2024-02-16 11:09:07 -0500

    reactHooks with useState

--- src/ClassState.jsx
+++ src/ClassState.jsx
@@ -4,7 +4,7 @@ class ClassState extends React.Component {
    render(){
     return(
         <div>
-            <h2>Eliminar ClassState</h2>
+            <h2>Eliminar {this.props.name}</h2>
             <p>Por favor, escribe el código de seguridad.</p>
             <input placeholder="Código de Seguridad" />
             <button>Comprobar</button>
