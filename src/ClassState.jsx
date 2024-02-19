14a9b042fbced0c45d9a7d6b6d859271b136ac84
Donovan Rojas <rojasdrm03@gmail.com>
2024-02-19 10:23:05 -0500

    efects and livecycles

--- src/ClassState.jsx
+++ src/ClassState.jsx
@@ -5,8 +5,19 @@ class ClassState extends React.Component {
         super(props);
         this.state = {
             error: false,
+            loading: false,
         };
     };
+    componentDidUpdate(){
+        console.log('Actialización');
+        if (!!this.state.loading) {
+            setTimeout(() => {
+                console.log("Haciendo la validación");
+                this.setState({loading:false});
+                console.log("Terminando la validación");
+            }, 3000);
+        }
+    }
    render(){
     return(
         <div>
@@ -15,10 +26,14 @@ class ClassState extends React.Component {
             {this.state.error && (
                 <p>Error: el código es incorrecto</p>
             )}
+            {this.state.loading && (
+                <p>Loading...</p>
+            )}
             <input placeholder="Código de Seguridad" />
             <button
                 onClick={() => 
-                    this.setState(prevState => ( {error: !prevState.error }))}
+                    this.setState({loading: true})
+                }
             >Comprobar</button>
         </div>
     );
@@ -30,3 +45,5 @@ export { ClassState };
 //una forma
 // onClick={() => 
 //     this.setState({ error: !this.state.error })}
+
+//this.setState(prevState => ( {error: !prevState.error }))
--- src/UseState.jsx
+++ src/UseState.jsx
@@ -1,7 +1,31 @@
 import React, { useState } from "react";
 
+const SECURITY_CODE = 'paradigma';
 function UseState({name}) {
+
+    const [value, setValue] = useState('');
     const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    React.useEffect(() => {
+        console.log("Empezamos el efecto");
+        if(!!loading){
+            setTimeout(() => {
+                console.log("Haciendo la validación");
+                if (value === SECURITY_CODE) {
+                    setLoading(false);
+                    setError(false);
+                } else {
+                    setError(true);
+                    setLoading(false);
+                }
+                // setLoading(false);
+                console.log("Terminando la validación");
+            }, 3000);
+        }
+        console.log("Terminamos el efecto");
+    }, [loading]);
+
     return(
         <div>
             <h2>Eliminar {name}</h2>
@@ -9,9 +33,21 @@ function UseState({name}) {
             {error && (
                 <p>Error: el código es incorrecto</p>
             )}
-            <input placeholder="Código de Seguridad" />
+            {loading && (
+                <p>Cargando...</p>
+            )}
+            <input 
+                placeholder="Código de Seguridad"
+                value={value}
+                onChange={(event) =>{
+                    setValue(event.target.value);
+                }} 
+            />
             <button
-                onClick={() => setError(!error)}
+                onClick={() => (
+                    setError(false),
+                    setLoading(!loading)
+                    )}
             >Comprobar</button>
         </div>
     );
